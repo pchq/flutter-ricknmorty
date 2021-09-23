e40ce5b1c353c10cfb464c5a30e2fd8ed0a1d43f
a.pilipchuk <@>
2021-09-23 11:47:45 +0400

    feat(filter): showModalBottomSheet

--- lib/presentation/pages/characters_page.dart
+++ lib/presentation/pages/characters_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ricknmorty/common/theme_config.dart';
+
 import 'package:ricknmorty/domain/model/character.dart';
 import 'package:ricknmorty/presentation/bloc/all_characters/all_characters_cubit.dart';
 import 'package:ricknmorty/presentation/widgets/character_card.dart';
@@ -26,7 +28,7 @@ class CharactersPage extends StatelessWidget {
         title: const Text('Rick\'n\'Morty'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _showFilter(context),
             icon: const Icon(Icons.filter_list),
           )
         ],
@@ -61,6 +63,153 @@ class CharactersPage extends StatelessWidget {
               },
             );
           },
+        ),
+      ),
+    );
+  }
+
+  void _showFilter(context) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (BuildContext context) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              child: Wrap(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Filter',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: ThemeConfig.lightGrey,
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 28,
+                        padding: EdgeInsets.zero,
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(
+                          Icons.close,
+                          color: ThemeConfig.lightGrey,
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: TextFormField(
+                      onChanged: (value) {},
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: 'Name',
+                        contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                        border: UnderlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                      children: [
+                        Text('Status'),
+                        Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CustomCheckBox(title: 'Alive'),
+                            CustomCheckBox(title: 'Dead'),
+                            CustomCheckBox(title: 'Unknown'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Column(
+                      children: [
+                        Text('Gender'),
+                        Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CustomCheckBox(title: 'Male'),
+                            CustomCheckBox(title: 'Female'),
+                            CustomCheckBox(title: 'Genderless'),
+                            CustomCheckBox(title: 'Unknown'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Column(
+                  //   children: [
+                  //     Text('Species'),
+                  //     Flex(
+                  //       direction: Axis.horizontal,
+                  //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //       children: [
+                  //         CustomCheckBox(title: 'Human'),
+                  //         CustomCheckBox(title: 'Alien'),
+                  //         CustomCheckBox(title: 'Robot'),
+                  //         CustomCheckBox(title: 'Humanoid'),
+                  //         CustomCheckBox(title: '...'),
+                  //       ],
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
+
+class CustomCheckBox extends StatefulWidget {
+  final String title;
+  CustomCheckBox({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _CustomCheckBoxState createState() => _CustomCheckBoxState();
+}
+
+class _CustomCheckBoxState extends State<CustomCheckBox> {
+  bool _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _value = !_value;
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: Text(widget.title),
+              ),
+              Icon(
+                _value ? Icons.check_box_outlined : Icons.check_box_outline_blank,
+              ),
+            ],
+          ),
         ),
       ),
     );
